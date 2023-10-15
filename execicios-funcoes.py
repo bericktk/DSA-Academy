53024392e3ec590ac530d44c089466acaa92ca25
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2023-10-15 19:08:27 -0300

    Update execicios-funcoes.py

--- execicios-funcoes.py
+++ execicios-funcoes.py
@@ -22,3 +22,48 @@ def elementos(elementos):
 elementos2 = ['terra', 'água']
 elementos(elementos2)
 print(elementos2)
+
+# Exercício 4 - Crie uma função que receba um argumento formal e uma possível lista de elementos. Faça duas chamadas 
+# à função, com apenas 1 elemento e na segunda chamada com 4 elementos
+def printNum(arg1, *lista):
+    print(arg1)
+    for i in lista:
+        print(i)
+    return
+printNum(200)
+printNum('A', 'B', 'C')
+
+# Exercício 5 - Crie uma função anônima e atribua seu retorno a uma variável chamada soma. A expressão vai receber 2 
+# números como parâmetro e retornar a soma deles
+soma = lambda num1, num2: num1 + num2
+print("A soma do produto é: ", soma(568, 257))
+
+# Exercício 6 - Execute o código abaixo e certifique-se que compreende a diferença entre variável global e local
+total = 0
+def soma( arg1, arg2 ):
+    total = arg1 + arg2; 
+    print ("Dentro da função o total é: ", total)
+    return total
+soma( 10, 20 )
+print ("Fora da função o total é: ", total)
+
+# Exercício 7 - Abaixo você encontra uma lista com temperaturas em graus Celsius
+# Crie uma função anônima que converta cada temperatura para Fahrenheit
+# Dica: para conseguir realizar este exercício, você deve criar sua função lambda, dentro de uma função 
+# (que será estudada no próximo capítulo). Isso permite aplicar sua função a cada elemento da lista
+# Como descobrir a fórmula matemática que converte de Celsius para Fahrenheit? Pesquise!!!
+Celsius = [39.2, 36.5, 37.3, 37.8]
+Fahrenheit = list(map(lambda c: (c * 9/5) + 32, Celsius))
+print (list(Fahrenheit))
+
+# Exercício 8 - Crie uma list comprehension que imprima o quadrado dos números de 1 a 10
+[x**2 for x in range(1,11)]
+
+# Exercício 9 - Crie uma list comprehension que imprima as palavras com a letra a no nome
+palavras = ["maça", "coiote", "banana", "terreno", "Python"]
+novasPalavras = [x for x in palavras if "a" in x]
+print(novasPalavras)
+
+# Exercício 10 - Crie uma list comprehension que imprima os números menores que 5 em um intervalo de 1 a 10
+numeros = [x for x in range(11) if x < 5]
+print(numeros)
